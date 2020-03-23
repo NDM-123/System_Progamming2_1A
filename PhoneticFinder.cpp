5a127db967e2c800a359a7956733b4bd5ebad514
NDM-123 <57434608+NDM-123@users.noreply.github.com>
2020-03-23 15:29:35 +0200

    Add files via upload
    adding functions without implementation

---
+++ PhoneticFinder.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "PhoneticFinder.hpp"
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+namespace phonetic {
+   string find(string a,string b) {
+  
+	return "";    
+   }
+}
+
+
